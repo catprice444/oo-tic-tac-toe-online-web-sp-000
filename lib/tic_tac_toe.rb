397a64adda9f8798d733f5643c3e6aef92743fbe
Catherine Price <catprice444@gmail.com>
2020-05-23 17:26:42 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,8 +3,7 @@ class TicTacToe
     @board = board || Array.new(9, " ")
   end
 
-  def WIN_COMBINATIONS
-    win_combinations = [
+  WIN_COMBINATIONS= [
       [0,1,2],
       [3,4,5],
       [6,7,8],
@@ -14,5 +13,5 @@ class TicTacToe
       [1,4,7],
       [2,5,8]
     ]
-  end
+  
 end
