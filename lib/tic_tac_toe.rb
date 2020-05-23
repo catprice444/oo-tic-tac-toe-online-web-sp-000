f1e9e35da1aca95e6f35ef8ad1c2800d8aa75967
Catherine Price <catprice444@gmail.com>
2020-05-23 17:41:25 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,8 +34,8 @@ def position_taken?(index)
   @board[index] != " " && @board[index] != ""
 end
 
-def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
+def valid_move?(index)
+  index.between?(0,8) && !position_taken?(@board, index)
 end
 
 def turn(board)
